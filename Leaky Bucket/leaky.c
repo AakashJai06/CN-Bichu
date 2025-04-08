d26ea2029a1799f0754cb3ab05258d51ab0f6c6a
Aakash J John <127650255+AakashJai06@users.noreply.github.com>
2025-04-08 17:53:14 +0530

    Create leaky.c

---
+++ Leaky Bucket/leaky.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int bucket_size, n, outgoing, incoming, store = 0;
+
+    printf("Enter bucket size: ");
+    scanf("%d", &bucket_size);
+    printf("Enter outgoing rate: ");
+    scanf("%d", &outgoing);
+    printf("Enter number of inputs: ");
+    scanf("%d", &n);
+
+    while (n--) {
+        printf("\nEnter incoming packet size: ");
+        scanf("%d", &incoming);
+        printf("Incoming packet size: %d\n", incoming);
+
+        if (incoming <= (bucket_size - store)) {
+            store += incoming;
+            printf("Bucket buffer size: %d out of %d\n", store, bucket_size);
+        } else {
+            printf("Dropped %d packet(s)\n", incoming - (bucket_size - store));
+            store = bucket_size;
+            printf("Bucket buffer size: %d out of %d\n", store, bucket_size);
+        }
+
+        store -= outgoing;
+        if (store < 0) store = 0;
+        printf("After outgoing, %d packet(s) left in buffer\n", store);
+    }
+    return 0;
+}
